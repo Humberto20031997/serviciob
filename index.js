90ffbfce1dc70a420c8a82dba603f3ad739b5574
Tsito12 <leniad_tr@hotmail.com>
2021-07-16 16:57:56 -0500

    Prueba API externa

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const async = require('async');
 
 app.get('/upcoming', (req, res) => {
     async.times(2, (i, callback) => {
-    var options = {
-        url: 'link api',
-        qs: {
-            'empresa': 'nom_estab',
-            'actividad': 'raz_social',
-            'codigo postal': 'cod_postal'
-        },
-    } 
+        const options = {
+            method: 'GET',
+            url: 'https://numbersapi.p.rapidapi.com/1492/year',
+            qs: {fragment: 'true', json: 'true'},
+            headers: {
+              'x-rapidapi-key': '9d4b049e43msh35fba3d6e9c94c7p1f601ejsn8a590d0b4aac',
+              'x-rapidapi-host': 'numbersapi.p.rapidapi.com',
+              useQueryString: true
+            }
+          };
     request(options, (error, response, body) => {
         var result = JSON.parse(body)
         var data = result.results;
@@ -20,6 +22,7 @@ app.get('/upcoming', (req, res) => {
     });
     }, (err, results) => {
         res.json(results);
+        console.log(err);
     });
 })
 
